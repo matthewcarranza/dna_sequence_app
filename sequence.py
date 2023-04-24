dad1f6e8fcbc0f0a1231ac441fee7ccf51b85493
matthewcarranza <98849589+matthewcarranza@users.noreply.github.com>
2023-04-23 23:23:32 -0700

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from PIL import Image
+import pandas as pd
+import altair as alt
 
 st.write("""
 # DNA Nucleotide Translator
@@ -13,7 +14,7 @@ st.header('Enter DNA Sequence')
 
 sequence_input = ">DNA Query\nGAACTGATAGCGATCGCATCGATCGACTAGCATCG\nGCCTATATCGCTACTCGACTACTAGCTAGCGATCGAGCT\nGATCTCGATATCTGCAGCGCTCGCCGTACGAGC"
 
-sequence = st.text_area("Sequence input", sequence_input, height=25)
+sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
 sequence = sequence[1:]
 sequence = ''.join(sequence)
@@ -26,3 +27,42 @@ st.header('INPUT (DNA Query)')
 sequence
 
 st.header('OUTPUT (DNA Nucleotide Count)')
+
+st.subheader('1. Print dictionary')
+def DNA_nucleotide_count(seq):
+  d = dict([
+            ('A',seq.count('A')),
+            ('T',seq.count('T')),
+            ('G',seq.count('G')),
+            ('C',seq.count('C'))
+            ])
+  return d
+  
+X = DNA_nucleotide_count(sequence)
+
+X_label = list(X)
+X_values = list(X,values())
+
+st.subheader('2. Print text')
+st.write(f"There are {str(S['A'])} adenine (A)\n
+          There are {str(S['T'])} thymine (T)\n
+          There are {str(S['G'])} guanine (G)\n
+          There are {str(S['C'])} cytosine (C)")
+          
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'nucleotide'})
+st.write(df)
+
+st.subheader('4. Display Bar Chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+
+p = p.properties(
+    width=alt.Step(80)
+)
+st.write(p)
