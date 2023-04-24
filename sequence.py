6fd191f23ba23bee24cd724007b3cbfeb8efbe84
matthewcarranza <98849589+matthewcarranza@users.noreply.github.com>
2023-04-23 23:08:15 -0700

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -8,3 +8,21 @@ Translates DNA sequences into their proper amino acid names.
 
 ***
 """)
+
+st.header('Enter DNA Sequence')
+
+sequence_input = ">DNA Query\nGAACTGATAGCGATCGCATCGATCGACTAGCATCG\nGCCTATATCGCTACTCGACTACTAGCTAGCGATCGAGCT\nGATCTCGATATCTGCAGCGCTCGCCGTACGAGC"
+
+sequence = st.text_area("Sequence input", sequence_input, height=25)
+sequence = sequence.splitlines()
+sequence = sequence[1:]
+sequence = ''.join(sequence)
+
+st.write("""
+***
+""")
+
+st.header('INPUT (DNA Query)')
+sequence
+
+st.header('OUTPUT (DNA Nucleotide Count)')
